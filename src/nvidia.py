418730392ace47d4b17e36ebe511a3b41cf213f8
Osman Coskun <osman.coskun@pardus.org.tr>
2024-01-22 16:33:57 +0300

    fix: new nvidia properties

--- src/nvidia.py
+++ src/nvidia.py
@@ -1,8 +1,6 @@
 import os
 import apt
 import json
-import package
-import subprocess
 
 nvidia_pci_id = "10DE"
 nvidia_pci_id_int = 0x10DE
@@ -10,8 +8,6 @@ nvidia_devices_yaml_path = "/../data/nvidia-pci.yaml"
 nvidia_devices_json_path = "/../data/nvidia-pci.json"
 drivers = {"current": "nvidia-driver", "470": "nvidia-tesla-470-driver"}
 nouveau = "xserver-xorg-video-nouveau"
-
-
 class PciDev:
     def __init__(self, vendor: str, device: str, cur_driver: str):
         self.vendor = vendor
@@ -20,43 +16,75 @@ class PciDev:
 
     def __str__(self):
         return f"{self.vendor:04x}:{self.device:04x}"
+
 class NvidiaDevice:
     def __init__(self,vendor_id:int=None,vendor_name:str=None,
                  device_id:str=None,device_name:str=None,
-                 driver_ver:str=None,driver_name:str=None
-                 ):
+                 driver_name:str=None,driver_version:str=None):
         self.vendor_id = vendor_id
         self.vendor_name = vendor_name
 
         self.device_id = device_id
         self.device_name = device_name
 
-        self.driver_ver = driver_ver
         self.driver_name = driver_name
+        self.driver_version = driver_version
     
+
+def get_pci_ids():
+    with open("/usr/share/misc/pci.ids","r") as f:
+        pci_ids = f.readlines()
+    devices = {}
+    cur_vendor = None
+    for line in pci_ids:
+        if line.startswith("#") or line.strip() == "":
+            continue
+        if not line.startswith("\t"):
+            vendor_id, vendor_name = line.strip().split(" ",1)
+            vendor_id = int(vendor_id,16)
+            devices[vendor_id] = {
+                "vendor_id" : vendor_id,
+                "vendor_name" : vendor_name.strip(),
+                "devices":{},
+            }
+            cur_vendor = vendor_id
+        else:
+            device_id,device_name = line.strip().split(" ",1)
+            device_id = int(device_id,16)
+            devices[cur_vendor]["devices"][device_id] = device_name.strip()
+    return devices
 
 def graphics():
     pci_dev_path = "/sys/bus/pci/devices/"
+    pci_ids = get_pci_ids()
     for paths, dirs, files in os.walk(pci_dev_path):
         if dirs:
             for dir in dirs:
                 vp = os.path.join(pci_dev_path,dir,"vendor")
-                vc = readfile(vp).strip()
+                vc = readfile(vp)
                 vc = int(vc,16)
+                vn = pci_ids[vc]['vendor_name']
 
                 if vc == nvidia_pci_id_int:
                     dp = os.path.join(pci_dev_path,dir,"device")
-                    dc = readfile(dp).strip()
+                    dc = readfile(dp)
                     dc = int(dc,16)
+                    dn = pci_ids[vc]["devices"][dc]
+                    
+                    
+                    drv_p = os.path.join(pci_dev_path,dir,"driver","module")
+                    orig_drv_p = os.readlink(drv_p)
+                    drv_c = os.path.basename(orig_drv_p)
+                    drv_ver_p = os.path.join(drv_p,"version")
+                    drv_ver_c = readfile(drv_ver_p)
+                    yield NvidiaDevice(vc,vn,dc,dn,drv_c,drv_ver_c)
 
-
-
+                
 def readfile(filepath:str=None):
     content = None
     if os.path.isfile(filepath):
-        f = open(filepath,"r")
-        content = f.read()
-        f.close()
+        with open(filepath,"r") as f:
+            content = f.read().strip()
     return content
 
 def get_dev_list():
