dab84af784049c4d20a58019080d597c79bf3090
Osman Coskun <osman.coskun@pardus.org.tr>
2024-07-15 23:33:09 +0300

    fix: missing translations

--- src/std_opr.py
+++ src/std_opr.py
@@ -2,9 +2,18 @@ import os
 import gi
 import dbus
 import subprocess
+import locale
+
 
 gi.require_version("Gtk", "3.0")
+
 from gi.repository import GLib, Gtk
+from locale import gettext as _
+
+APPNAME_CODE = "pardus-nvidia-installer"
+TRANSLATIONS_PATH = "/usr/share/locale/"
+locale.bindtextdomain(APPNAME_CODE, TRANSLATIONS_PATH)
+locale.textdomain(APPNAME_CODE)
 
 
 def start_prc(self, params):
@@ -53,7 +62,7 @@ def on_process_stdext(pid, stat, self):
     print(stat)
     self.ui_status_progressbar.set_fraction(1)
     if stat == 0:
-        self.ui_status_progressbar.set_text("Operation completed successfully")
+        self.ui_status_progressbar.set_text(_("Operation completed successfully"))
         if self.apt_opr != "update":
             if self.ui_confirm_dialog:
                 dlg_res = self.ui_confirm_dialog.run()
@@ -74,6 +83,6 @@ def on_process_stdext(pid, stat, self):
             self.ui_confirm_dialog.destroy()
         self.create_gpu_drivers()
     else:
-        self.ui_status_progressbar.set_text("An error occured.")
+        self.ui_status_progressbar.set_text(_("An error occured."))
         return False
     self.ui_main_window.set_sensitive(True)
